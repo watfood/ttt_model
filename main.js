fb22671f2dfae6ede092e23ec6972f77f7a8c088
Duane Than <eventsco@aol.com>
2016-01-26 12:59:44 -0800

    TTT Model work prelunch

--- main.js
+++ main.js
@@ -11,6 +11,20 @@ var board = [
 ];
 
 // 1. Start The Game
+//   inputs:
+//   - current player, board, won
+//   output: board is emptied, player is x, won is false
+//
+
+var startGame = function() {}
+won = false;
+currentPlayer = "X"
+board = [
+  "", "", "", // 0, 1, 2
+  "", "", "", // 3, 4 ,5
+  "", "", "" // 6, 7, 8
+];
+
 // 2. Move + changes turn
 //    inputs:
 //    -whose turn it is,
@@ -24,7 +38,7 @@ var move = function(cellIndex) {
   if (gameWon()) {
     won = true;
   } else {
-    if currentPlayer === "X" {
+    if (currentPlayer === "X") {
       currentPlayer = "O";
     } else {
       currentPlayer = "X";
@@ -35,6 +49,25 @@ var move = function(cellIndex) {
 
 
 // 3. Win the Game
-var gameWon = function() {
+//  inputs:
+//  -board
+//
+//  outputs
+//  true or false
+var gameWon = function(){
+  if (
+  ((board[0] === board[1]) && (board[0]=== board[2]) && board[0] !=="") ||
+  ((board[3] === board[4]) && (board[3]=== board[5]) && board[3] !=="") ||
+  ((board[6] === board[7]) && (board[6]=== board[8]) && board[6] !=="")
+  ){
+    return true;
+    } else {
+      return false;
+  }
+};
 
-}
+/* RENDER OUR VIEW */
+
+var render = function() {
+
+};
